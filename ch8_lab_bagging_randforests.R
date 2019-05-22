31ac823054ed40eed484794602a759d873f762c4
Rudy <rudysw05@knights.ucf.edu>
2019-05-22 15:38:49 -0400

    update rf code

--- ch8_lab_bagging_randforests.R
+++ ch8_lab_bagging_randforests.R
@@ -10,3 +10,12 @@ bag.pred = predict(bag.boston, newdata=boston.test)
 plot(boston.test.y, bag.pred)
 bag.mse = mean((boston.test.y - bag.pred)^2)
 bag.mse #14.8
+
+#build random forest model
+set.seed(1)
+rf.boston = randomForest(medv~., data=Boston, subset=boston.train, mtry=6, importance=T)
+##by default mtry = p/3 for regression trees
+rf.pred = predict(rf.boston, newdata=boston.test)
+rf.mse = mean((boston.test.y - rf.pred)^2)
+rf.mse
+importance(rf.boston)
