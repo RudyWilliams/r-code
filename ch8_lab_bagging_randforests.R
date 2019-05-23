abdc88ee219c2ad0c31e85a6749a66be5a88c1af
Rudy <rudysw05@knights.ucf.edu>
2019-05-23 10:28:54 -0400

    included boosting in this script as well

--- ch8_lab_bagging_randforests.R
+++ ch8_lab_bagging_randforests.R
@@ -20,3 +20,35 @@ rf.mse = mean((boston.test.y - rf.pred)^2)
 rf.mse
 importance(rf.boston)
 varImpPlot(rf.boston)
+
+
+#perform boosting on the dataset
+library(gbm)
+set.seed(1)
+
+boost.boston = gbm(medv~., data=Boston[boston.train,], distribution='gaussian',
+                   n.trees=5000, interaction.depth=4)
+##gbm doesn't have a subset option
+##use 'gaussian' (sq loss) for regression & 'bernoulli' if a binary classification problem
+summary(boost.boston)
+
+#partial dependence plots
+plot(boost.boston, i='rm', main='Partial Dependence Plot: "rm"', ylab='medv')
+plot(boost.boston, i='lstat', main='Partial Dependence Plot: "lstat"', ylab='medv')
+
+boost.pred = predict(boost.boston, newdata=boston.test, n.trees=5000)
+##n.trees says to use the first n.trees iterations of the boosting sequence
+boost.mse = mean((boston.test.y - boost.pred)^2)
+boost.mse
+
+mses = c()
+x = c(10,25,50,100,200,300,400,500,600,700,800,900,1000)#,1500,3000,5000)
+#see the affect of n.trees (overfitting)
+for (i in x){
+  boost.pred = predict(boost.boston, newdata=boston.test, n.trees=i)
+  boost.mse = mean((boston.test.y - boost.pred)^2)
+  mses = append(mses, boost.mse)
+}
+par(mfrow=c(1,1))
+plot(x, mses, xlab='Number of Trees', ylab='Test MSE', type='b', col='blue')
+abline(v=100)
